99431f7f590bfb3023d342b48aad9707882866d8
bekure27 <bekure27@gmail.com>
2022-09-05 19:16:48 +0300

    complete repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(word , num) {
+ let repeatWord = "";
+ if(num <= -1){
+    return 'ERROR';
+ }
+ 
+ 
+ while(num > 0){
+    repeatWord+=word;
+    num--;
+    
+ }
+ return repeatWord;
+ 
 };
-
+repeatString('hey',3);
 // Do not edit below this line
 module.exports = repeatString;
