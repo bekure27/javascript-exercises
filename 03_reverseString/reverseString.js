453f70cab4e7218d9b676461a44027645ec297b0
bekure27 <bekure27@gmail.com>
2022-09-05 20:11:25 +0300

    complete reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+var reverseWord = "";
+for(let i = word.length; i>=0; i--){
+reverseWord = reverseWord.concat(word.charAt(i));
+}
+return reverseWord;
 };
-
+reverseString('hello');
 // Do not edit below this line
 module.exports = reverseString;
