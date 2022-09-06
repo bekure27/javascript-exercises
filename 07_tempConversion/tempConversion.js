cec445d0319edccf0c3e8f4aba3b899ede6f052f
bekure27 <bekure27@gmail.com>
2022-09-06 15:53:38 -0700

    complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let c = (ftemp - 32)*(5/9);
+   c = Math.round(c*10)/10;
+   return c;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+let f = ctemp*(9/5) + 32;
+// f = f.toFixed(1);
+f = Math.round(f*10)/10;
+return f;
 };
 
 // Do not edit below this line
