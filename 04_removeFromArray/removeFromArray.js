3201e6dbda1830d107a1353ef636517cf31fb3df
bekure27 <bekure27@gmail.com>
2022-09-06 11:31:27 +0300

    2 test fail with logic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayElement,...value) {
+      newArrayElement = [];
+    for(let i=0; i<=arrayElement.length; i++){
+        if(value.includes(arrayElement[i])){
+         arrayElement.splice(i, value.length);
+        }
+}
 
+
+return arrayElement;
+// return console.log(arrayElement)
 };
-
+// removeFromArray([1, 2, 3, 4], 7, 2);
 // Do not edit below this line
 module.exports = removeFromArray;
